a3555bfc57a6a746bd867d2e73f271abb1d0da33
Mehnazahmed <mehnazchowdhury613@gmail.com>
2022-11-03 14:49:10 +0600

    added

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,17 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import CheckOut from "../../Pages/CheckOut/CheckOut";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
 
- const router = createBrowserRouter([
+const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
-        children:[
+        children: [
             {
-                path:'/',
-                element:<Home></Home>
+                path: '/',
+                element: <Home></Home>
             },
             {
                 path: '/login',
@@ -20,6 +21,11 @@ import SignUp from "../../Pages/SignUp/SignUp";
             {
                 path: '/signup',
                 element: <SignUp></SignUp>
+            },
+            {
+                path: '/checkout/:id',
+                element: <CheckOut></CheckOut>,
+                loader: ({ params }) => fetch(`http://localhost:5000/services${params.id}`)
             }
 
         ]
